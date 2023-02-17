269d169c49dc9c2dbe7fcd25481084c39bb53ac8
Jackson Earl Rudoff <62763243+jer164@users.noreply.github.com>
2023-02-16 19:50:48 -0500

    created the notebook

---
+++ src/inclass_week_5.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+print("They got Spunchbob codin")
+
+# COMMAND ----------
+
+def number_printer(input):
+    if input > 4:
+        print("wow what a cool number")
+    elif input == 4:
+        print("it's okay I guess")
+    else:
+        print(r"bad number >:(")
+        
+
+# COMMAND ----------
+
+number_printer(2)
+
+# COMMAND ----------
+
